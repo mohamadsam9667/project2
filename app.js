8f4b60cbc895f3829b1acf8a8cb9e0ac05d2041b
mohamadsam9667 <146078669+mohamadsam9667@users.noreply.github.com>
2024-10-21 08:43:14 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,70 +1,134 @@
 
+let yourFood = [
+  'قرمه سبزی',
+  'سوپ',
+  'آش',
+  'استانبلی',
+  'ماکارانی',
+  'فلافل',
+  'سیب زمینی کوبیده',
+  'سوسیس تخم مرغ',
+  'تخم مرغ',
+  'مرغ',
+  'ماهی',
+  'قیمه',
+  'سیب زمینی و تخم مرغ',
+  'لوبیا چیتی',
+  'عدسی',
+  'کرفس',
+  'کوکو سبزی',
+  'کوکو سیب زمینی',
+  'بادمجون گوجه',
+  'یتیم چه',
+  'جوجه',
+  'حاضری',
+  'کتلت'
+];
 
 
-let inputTag=document.getElementById('input-field')
-let deletBtn=document.getElementById('btn-delete')
-let listedShow=document.getElementById('listed')
-let saveBtn=document.getElementById("btn-save")
+let economicFood = [
+  'اشکنه',
+  'خوراک لوبیا',
+  'املت',
+  'آب دوغ خیار',
+  'میرزا قاسمی',
+  'عدسی',
+  'خوراک سبزیجات',
+  'نون و پنیر و سبزی',
+  'نخود آب',
+  'کشک بادمجان',
+  'خوراک بادمجان',
+  'ماش پلو',
+  'دمی گوجه',
+  'خوراک عدس و سیب زمینی',
+  'کله جوش',
+  'سیب زمینی تنوری',
+  'پیاز داغ پلو',
+  'خوراک کدو',
+  'خوراک قارچ',
+  'آش رشته',
+  'آش جو',
+  'پلو عدس',
+  'خوراک نخود فرنگی',
+  'دلمه',
+  'پوره سیب زمینی',
+  'سوپ جو',
+  'کتلت مرغ',
+  'لوبیا پلو',
+  'دو پیازه آلو',
+  'خوراک قارچ و سیب زمینی',
+  'اشکنه تخم مرغ',
+  'اشکنه سیب زمینی',
+  'خوراک هویج و سیب زمینی',
+  'خوراک سیب زمینی و کرفس',
+  'خوراک کدو و گوجه',
+  'سیب زمینی آب پز با کره',
+  'پوره هویج',
+  'بلغور پلو',
+  'سبزی پلو با تخم مرغ',
+  'کوفته ریزه'
+];
 
-let boxsArray=document.querySelectorAll('.color-box')
-let targetBox=document.getElementById('color-select')
+let nonEconomicFood = [
+  'کباب',
+  'زرشک پلو با مرغ',
+  'باقالی پلو با ماهیچه',
+  'خورشت فسنجان',
+  'ماهی',
+  'چلو گوشت',
+  'جوجه کباب',
+  'خوراک میگو',
+  'مرصع پلو',
+  'قلیه ماهی',
+  'خوراک زبان',
+  'خورشت آلو مسما',
+  'خورشت خلال کرمانشاهی',
+  'ته‌چین مرغ',
+  'کلم پلو شیرازی',
+  'بریان اصفهانی',
+  'خوراک پاچین',
+  'چلو گردن',
+  'کباب سلطانی',
+  'استیک'
+];
+
+
+
+let allFood = [
+  ...yourFood,      // غذاهای موجود در لیست شما
+  ...economicFood,  // غذاهای اقتصادی
+  ...nonEconomicFood // غذاهای غیر اقتصادی
+];
+
+
+
+let btn=document.querySelector('.btn')
+let h1Elem=document.querySelector('.title')
 
 
 
 
-boxsArray.forEach(function(box){
-    
-    let MainColor=box.style.backgroundColor
-    box.addEventListener('click',function(){
-        inputTag.style.backgroundColor=MainColor
-        
-        
-    })
-    
-    
+// console.log(allFood)
+
+btn.addEventListener('click',function(){
+  let numberRand=(Math.floor(Math.random()*allFood.length))
+console.log(numberRand)
+h1Elem.innerHTML=`${allFood[numberRand] }`
 })
-
-function newElem (){
-    
-    let colorInput=inputTag.style.backgroundColor
-    
-    let newDiv=document.createElement('div');
-    let newP=document.createElement('p')
-        newDiv.classList.add('cardShadow-smRounded');
-        newDiv.classList.add('shadow-sm');
-        newDiv.classList.add('rounded');
-        newDiv.style.backgroundColor=colorInput
-        newDiv.addEventListener('click',rmover)
-        newP.classList.add('card-text')
-        newP.classList.add('p-3')
-   
-       
-        newP.innerHTML=inputTag.value
-        newDiv.appendChild(newP)
-        listedShow.append(newDiv)
-        inputTag.value=''
-        inputTag.style.backgroundColor='#fff'
-}   
-
-
-
-inputTag.addEventListener('keydown',function(event){
-    if(event.keyCode === 13){
-        newElem();
-    }
-})
-saveBtn.addEventListener('click',newElem)
+// btn.addEventlistener('click',function(){
+//   console.log("Sa")
+// })
 
 
 
 
 
-deletBtn.addEventListener('click',function(){
-    inputTag.value=''
-    inputTag.style.backgroundColor='#fff'
-})
 
 
-function rmover(event){
-    event.target.parentElement.remove()
-}
+
+
+
+
+
+
+
